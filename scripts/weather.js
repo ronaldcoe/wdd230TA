2a2f3001b94b326c4e7473c5406ce003c636d53c
Ronald Coello <ronaldcoello85@gmail.com>
2023-02-10 12:53:54 -0700

    more css

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,36 +1,55 @@
 function getTime() {
-    let today = new Date();
-    let time = today.getHours();
-    return time
+  let today = new Date();
+  let time = today.getHours();
+  return time;
 }
 function convertToJson(response) {
-    if (response.ok) {
-        return response.json();
-    } else {
-        console.log('error: ', response);
-    }
+  if (response.ok) {
+    return response.json();
+  } else {
+    console.log("error: ", response);
+  }
 }
 
-function newSearch(){
-
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/guayaquil?unitGroup=us&key=EWQF52DJKF9B9NEMA45J3VZGW&contentType=json`;
-    fetch(url).then(convertToJson).then(displayCurrentTemp);
+function newSearch() {
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/guayaquil?unitGroup=us&key=EWQF52DJKF9B9NEMA45J3VZGW&contentType=json`;
+  fetch(url).then(convertToJson).then(displayCurrentTemp);
 }
-
 
 function displayCurrentTemp(data) {
-    let img = data.days[0].hours[getTime()-1].icon;
-    let f = document.querySelector(".temp").innerHTML = data.days[0].hours[getTime()-1].temp;
-    let c = Math.ceil((f - 32) * 0.5556);
-    
-    document.querySelector('.imgweather').src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/2nd%20Set%20-%20Color/${img}.png`;
+  let img = data.days[0].hours[getTime() - 1].icon;
+  let f = (document.querySelector(".temp").innerHTML =
+    data.days[0].hours[getTime() - 1].temp);
+  let c = f
 
-    document.querySelector(".desc").innerHTML = data.days[0].hours[getTime()-1].conditions;
- 
-    document.querySelector(".temp").innerHTML = c + "<span>&#8451;</span>";
-    document.querySelector(".windvalue").innerHTML = data.days[0].hours[getTime()-1].windspeed;
+  document.querySelector(
+    ".imgweather"
+  ).src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/2nd%20Set%20-%20Color/${img}.png`;
+
+  document.querySelector(".desc").innerHTML = data.days[0].hours[getTime() - 1].conditions;
+
+  document.querySelector(".temp").innerHTML = c + "<span>&#8457;</span>";
+  let wind = document.querySelector(".windvalue").innerHTML = data.days[0].hours[getTime() - 1].windspeed;
 
 
+
+  
+    let windChillSpan = document.querySelector(".windchill");
+
+    // Set up the wind chill content
+    let windchill = 'N/A';
+    if (wind >= 3.0 && c <= 50){
+        let chill = Math.round((35.74 + (0.6215 * c))-(35.75 * Math.pow(wind,0.16)) + (0.4275*c*Math.pow(wind,0.16)));
+        windchill = `${chill}`;
+    }
+
+    // Write out the dynamic content
+    windChillSpan.innerHTML = windchill;
 }
 
-newSearch()
+newSearch();
+
+
+
+
+
